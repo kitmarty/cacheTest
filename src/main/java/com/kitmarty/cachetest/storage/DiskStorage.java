1dc21ef74ee34e241a05e5639581c574383969e9
kitmarty <nikita.martyanov@gmail.com>
2018-04-24 09:57:06 +0300

    changed disk storage logic and implementation methods;

--- src/main/java/com/kitmarty/cachetest/storage/DiskStorage.java
+++ src/main/java/com/kitmarty/cachetest/storage/DiskStorage.java
@@ -1,88 +1,102 @@
 package com.kitmarty.cachetest.storage;
 
-import java.io.*;
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Objects;
 import java.util.Optional;
 
-public class DiskStorage<K, V extends Serializable> implements Storage<K, V> {
+public class DiskStorage<K, V extends Serializable>
+        implements Storage<K, V> {
 
+    private static final String FILE_EXTENSION = ".ser";
+
+    private static int storageCounter = 0;
+
+    private int storageID;
+    private int elementID;
     private final String storagePath;
+    private final HashMap<K, Integer> storageHash = new HashMap<>();
 
     public DiskStorage(String storagePath) {
         Objects.requireNonNull(storagePath, "objectStorage");
         this.storagePath = storagePath;
+        this.storageID = ++storageCounter;
     }
 
     public DiskStorage() {
         this.storagePath = System.getProperty("user.dir");
+        this.storageID = ++storageCounter;
     }
 
     @Override
     public Optional<V> put(K key, V value) {
-        File file = new File(storagePath, key.toString() + ".ser");
-        ObjectOutputStream oos = null;
+        storageHash.put(key, ++elementID);
+        Path filepath = Paths.get(storagePath, String.valueOf(storageID) + "_" + (String.valueOf(elementID)) + FILE_EXTENSION);
 
         try {
-            oos = new ObjectOutputStream(
-                    new BufferedOutputStream(
-                            new FileOutputStream(file)));
-            oos.writeObject(value);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            if (oos != null) {
-                try {
-                    oos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            Files.write(filepath, SerializationUtils.serialize(value));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
         return Optional.empty();
     }
 
     @Override
     public Optional<V> get(K key) {
-        File file = new File(storagePath, key.toString() + ".ser");
-        ObjectInputStream ois = null;
-        V valueToReturn = null;
-        if (file.exists()) {
+
+        Integer link = storageHash.get(key);
+        Optional<V> valueToReturn = Optional.empty();
+
+        if (link != null) {
+            Path filepath = Paths.get(storagePath, String.valueOf(storageID) + "_" + (String.valueOf(link)) + FILE_EXTENSION);
+
             try {
-                ois = new ObjectInputStream(
-                        new BufferedInputStream(
-                                new FileInputStream(file)));
-                valueToReturn = (V) ois.readObject();
-            } catch (Exception e) {
+                valueToReturn = Optional.ofNullable(SerializationUtils.deserialize(Files.readAllBytes(filepath)));
+            } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                if (ois != null) {
-                    try {
-                        ois.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
         }
-        return Optional.ofNullable(valueToReturn);
+
+        return valueToReturn;
     }
 
     @Override
     public Optional<V> remove(K key) {
-        File file = new File(storagePath, key.toString() + ".ser");
-        Optional<V> valueToReturn = Optional.empty();
-        if (file.exists()) {
-            valueToReturn = get(key);
-            file.delete();
+
+        Integer link = storageHash.get(key);
+        Optional<V> valueToReturn = get(key);
+
+        if (link != null) {
+            storageHash.remove(key);
+            Path filepath = Paths.get(storagePath, String.valueOf(storageID) + "_" + (String.valueOf(link)) + FILE_EXTENSION);
+            try {
+                Files.deleteIfExists(filepath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
         return valueToReturn;
     }
 
     public void clear() {
-        File dir = new File(this.storagePath);
-        File[] filesToRemove = dir.listFiles((dir1, filename) -> filename.endsWith(".ser"));
-        for (File file : filesToRemove) {
-            file.delete();
+        try {
+            DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(storagePath), "*" + FILE_EXTENSION);
+            for (Path entry : stream) {
+                Files.deleteIfExists(entry);
+            }
+            stream.close();
+            storageHash.clear();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
